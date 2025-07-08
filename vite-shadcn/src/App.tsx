0ff492d03fe766e2242cc7444479dc6075ac6b30
mabuhanifa <moh.abuhanifa@gmail.com>
2025-07-09 01:38:08 +0600

    shadcn components added

--- vite-shadcn/src/App.tsx
+++ vite-shadcn/src/App.tsx
@@ -1,3 +1,4 @@
+import { Blog7 } from "./components/blocks/blog7";
 import { ModeToggle } from "./components/mode-toggle";
 import { Button } from "./components/ui/button";
 
@@ -6,6 +7,7 @@ export default function App() {
     <div className="flex min-h-svh flex-col items-center justify-center ">
       <Button>Click me</Button>
       <ModeToggle />
+      <Blog7 />
     </div>
   );
 }
