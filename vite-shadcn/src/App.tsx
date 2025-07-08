3b9baba0d2822a6bcb363de75e3ea45aacf7e73f
mabuhanifa <moh.abuhanifa@gmail.com>
2025-07-08 22:54:19 +0600

    theme provider added

--- vite-shadcn/src/App.tsx
+++ vite-shadcn/src/App.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "./components/mode-toggle";
 import { Button } from "./components/ui/button";
 
 export default function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center ">
       <Button>Click me</Button>
+      <ModeToggle />
     </div>
   );
 }
